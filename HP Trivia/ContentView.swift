4f14ee4382811778b898684e41793d4f96c541e8
MarcCDJ <marccruzdj@gmail.com>
2023-08-10 17:41:28 -0400

    Add game play views

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State private var animateViewsIn = false
     @State private var showInstructions = false
     @State private var showSettings = false
+    @State private var playGame = false
 
     var body: some View {
         GeometryReader { geo in
@@ -117,7 +118,7 @@ struct ContentView: View {
                         VStack {
                             if animateViewsIn {
                                 Button {
-                                    // Start new game
+                                    playGame.toggle()
                                 } label: {
                                     Text("Play")
                                         .font(.largeTitle)
@@ -137,6 +138,9 @@ struct ContentView: View {
                                     }
                                 }
                                 .transition(.offset(y: geo.size.height / 3))
+                                .fullScreenCover(isPresented: $playGame) {
+                                    Gameplay()
+                                }
                             }
                         }
                         .animation(

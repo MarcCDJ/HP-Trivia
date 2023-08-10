28a2daa8e1477a2dada4f2957186f6eff99869e0
MarcCDJ <marccruzdj@gmail.com>
2023-08-10 08:26:36 -0400

    Add animation and audio

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -6,8 +6,13 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var scalePlayButton = false
+    @State private var moveBackgroundImage = false
+
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -15,28 +20,38 @@ struct ContentView: View {
                     .resizable()
                     .frame(width: geo.size.width * 3, height: geo.size.height)
                     .padding(.top, 3)
-                
+                    .offset(x: moveBackgroundImage
+                        ? geo.size.width / 1.1
+                        : -geo.size.width / 1.1)
+                    .onAppear {
+                        withAnimation(
+                            .linear(duration: 60)
+                                .repeatForever()) {
+                                    moveBackgroundImage.toggle()
+                        }
+                    }
+
                 VStack {
                     VStack {
                         Image(systemName: "bolt.fill")
                             .font(.largeTitle)
                             .imageScale(.large)
-                        
+
                         Text("HP")
                             .font(.custom(Constants.hpFont, size: 70))
                             .padding(.bottom, -50)
-                        
+
                         Text("Trivia")
                             .font(.custom(Constants.hpFont, size: 60))
                     }
                     .padding(.top, 70)
-                    
+
                     Spacer()
-                    
+
                     VStack {
                         Text("Recent Scores")
                             .font(.title2)
-                        
+
                         Text("33")
                         Text("27")
                         Text("15")
@@ -46,12 +61,12 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .background(.black.opacity(0.7))
                     .cornerRadius(15)
-                    
+
                     Spacer()
-                    
+
                     HStack {
                         Spacer()
-                        
+
                         Button {
                             // Show instructions screen
                         } label: {
@@ -60,9 +75,9 @@ struct ContentView: View {
                                 .foregroundColor(.white)
                                 .shadow(radius: 5)
                         }
-                        
+
                         Spacer()
-                        
+
                         Button {
                             // Start new game
                         } label: {
@@ -75,9 +90,17 @@ struct ContentView: View {
                                 .cornerRadius(7)
                                 .shadow(radius: 5)
                         }
-                        
+                        .scaleEffect(scalePlayButton ? 1.2 : 1)
+                        .onAppear {
+                            withAnimation(
+                                .easeInOut(duration: 1.3)
+                                    .repeatForever()) {
+                                scalePlayButton.toggle()
+                            }
+                        }
+
                         Spacer()
-                        
+
                         Button {
                             // Show settings screen
                         } label: {
@@ -86,17 +109,38 @@ struct ContentView: View {
                                 .foregroundColor(.white)
                                 .shadow(radius: 5)
                         }
-                        
+
                         Spacer()
                     }
                     .frame(width: geo.size.width)
-                    
+
                     Spacer()
                 }
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear {
+            playAudio()
+        }
+        .onTapGesture {
+            toggleAudio()
+        }
+    }
+    
+    private func playAudio() {
+        let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
+        audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        audioPlayer.numberOfLoops = Constants.audioInfinity
+        audioPlayer.play()
+    }
+    
+    private func toggleAudio() {
+        if audioPlayer.isPlaying {
+            audioPlayer.stop()
+        } else {
+            audioPlayer.play()
+        }
     }
 }
 

6a462f4189ed08a42cc5cb33a3d22995bd5c79ff
MarcCDJ <marccruzdj@gmail.com>
2023-08-11 14:27:23 -0400

    Handle user interaction

--- HP Trivia/Gameplay.swift
+++ HP Trivia/Gameplay.swift
@@ -8,15 +8,14 @@
 import SwiftUI
 
 struct Gameplay: View {
+    @Environment(\.dismiss) private var dismiss
     @State private var animateViewsIn = false
     @State private var tappedCorrectAnswer = false
     @State private var hintWiggle = false
     @State private var scaleNextButton = false
     @State private var movePointsToScore = false
-    
-    private var wiggleAnimation: Animation {
-        .easeInOut(duration: 0.1).repeatCount(9).delay(5).repeatForever()
-    }
+    @State private var revealHint = false
+    @State private var revealBook = false
 
     var body: some View {
         GeometryReader { geo in
@@ -27,12 +26,11 @@ struct Gameplay: View {
                     .overlay(Rectangle().foregroundColor(.black.opacity(0.8)))
 
                 VStack {
-                    
                     // MARK: Controls
-                    
+
                     HStack {
                         Button("End Game") {
-                            // TODO: End game
+                            dismiss()
                         }
                         .buttonStyle(.borderedProminent)
                         .tint(.red.opacity(0.5))
@@ -45,7 +43,7 @@ struct Gameplay: View {
                     .padding(.vertical, 30)
 
                     // MARK: Question
-                    
+
                     VStack {
                         if animateViewsIn {
                             Text("Who is Harry Potter?")
@@ -75,11 +73,31 @@ struct Gameplay: View {
                                     .transition(.offset(x: -geo.size.width / 2))
                                     .onAppear {
                                         withAnimation(
-                                            wiggleAnimation
+                                            .easeInOut(duration: 0.1).repeatCount(9).delay(5).repeatForever()
                                         ) {
                                             hintWiggle = true
                                         }
                                     }
+                                    .onTapGesture {
+                                        withAnimation(.easeOut(duration: 1)) {
+                                            revealHint = true
+                                        }
+                                    }
+                                    .rotation3DEffect(
+                                        .degrees(revealHint ? 1440 : 0),
+                                        axis: (x: 0.0, y: 1.0, z: 0.0)
+                                    )
+                                    .scaleEffect(revealHint ? 5 : 1)
+                                    .opacity(revealHint ? 0 : 1)
+                                    .offset(x: revealHint ? geo.size.width / 2 : 0)
+                                    .overlay(
+                                        Text("The Boy Who _____")
+                                            .padding(.leading, 33)
+                                            .minimumScaleFactor(0.5)
+                                            .multilineTextAlignment(.center)
+                                            .opacity(revealHint ? 1 : 0)
+                                            .scaleEffect(revealHint ? 1.33 : 1)
+                                    )
                             }
                         }
                         .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
@@ -102,11 +120,31 @@ struct Gameplay: View {
                                     .transition(.offset(x: geo.size.width / 2))
                                     .onAppear {
                                         withAnimation(
-                                            wiggleAnimation
+                                            .easeInOut(duration: 0.1).repeatCount(9).delay(5).repeatForever()
                                         ) {
                                             hintWiggle = true
                                         }
                                     }
+                                    .onTapGesture {
+                                        withAnimation(.easeOut(duration: 1)) {
+                                            revealBook = true
+                                        }
+                                    }
+                                    .rotation3DEffect(
+                                        .degrees(revealBook ? 1440 : 0),
+                                        axis: (x: 0.0, y: 1.0, z: 0.0)
+                                    )
+                                    .scaleEffect(revealBook ? 5 : 1)
+                                    .opacity(revealBook ? 0 : 1)
+                                    .offset(x: revealBook ? -geo.size.width / 2 : 0)
+                                    .overlay(
+                                        Image("hp1")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .padding(.trailing, 33)
+                                            .opacity(revealBook ? 1 : 0)
+                                            .scaleEffect(revealBook ? 1.33 : 1)
+                                    )
                             }
                         }
                         .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)

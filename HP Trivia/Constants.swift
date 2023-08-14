4409fe32732f3de2f8dfc43ed692db0b7e8c5fca
MarcCDJ <marccruzdj@gmail.com>
2023-08-14 12:15:04 -0400

    Add persistance

--- HP Trivia/Constants.swift
+++ HP Trivia/Constants.swift
@@ -35,3 +35,10 @@ extension Button {
             .foregroundColor(.white)
     }
 }
+
+extension FileManager {
+    static var documentsDirectory: URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
